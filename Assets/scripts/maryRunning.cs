d5af9be0caf3c72a7c495a093767d9c9671206d3
MAIN\eeo4537 <edwardOpich3@gmail.com>
2017-10-06 10:41:16 -0400

    Initial Commit

---
+++ Assets/scripts/maryRunning.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class maryRunning : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// move to the right
+		transform.Translate (0.1f, 0, 0);
+
+		// hits wall, start moving left
+		if(transform.position.x >= 4)
+		{
+			transform.rotation = Quaternion.Euler (0, 180, 0);
+		}
+
+		// hits other wall, start moving right
+		if (transform.position.x <= -4) 
+		{
+			transform.rotation = Quaternion.Euler (0, 0, 0);
+		}
+	}
+}
