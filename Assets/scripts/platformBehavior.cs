434b981a0fb4dc2224ae2cf8edb654273cb4c1cc
MAIN\eeo4537 <edwardOpich3@gmail.com>
2017-10-11 11:01:52 -0400

    Basic Falling Blocks
    -Pressing the Spacebar will now cause the block attatched to the crane to fall to the ground

---
+++ Assets/scripts/platformBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformBehavior : MonoBehaviour {
+
+	public bool isOnCrane;
+	public bool isFalling;
+	public bool isSettled;
+
+	// Use this for initialization
+	void Start ()
+	{
+		isOnCrane = true;
+		isFalling = false;
+		isSettled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space) && !isFalling && !isSettled)
+		{
+			isOnCrane = false;
+			isFalling = true;
+		}
+
+		if(isFalling)
+		{
+			transform.Translate(0.0f, -0.1f, 0.0f);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		isFalling = false;
+		isSettled = true;
+
+		//GameObject newBlock = Instantiate(gameObject);
+		//newBlock.transform.position.Set(0.0f, 2.96f, 0.0f);
+	}
+}
