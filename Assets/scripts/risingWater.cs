341fa7b17c69bdfa584e2c83d5ce61f4ff3c1afe
Edward Opich <edwardOpich3@gmail.com>
2017-10-13 14:12:03 -0400

    Rising Water
    -Also removed all 3D Colliders and replaced those necessary with 2D Colliders

---
+++ Assets/scripts/risingWater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class risingWater : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.position.Set(0.0f, -10.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(new Vector3(0.0f, 0.1f) * Time.deltaTime);
+	}
+}
